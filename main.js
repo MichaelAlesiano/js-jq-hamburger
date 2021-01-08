877eb311cc690cc6fd24e6baf2d476c7e7eddaba
Michael <michael.alesiano@gmail.com>
2021-01-08 14:45:08 +0100

    Finished project

---
+++ main.js
@@ -0,0 +1,10 @@
+// Hamburger menu:
+// mostrare / nascondere il menu principale
+
+$(document).ready(function(){
+
+    $('i').click(function(){
+        $('.hamburger-menu').toggle();
+    });
+
+});
